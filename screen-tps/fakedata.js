036fc5b8d0697a9c60d91552ed5bb74943e4028d
ihab abadi <ihab@utopios.net>
2022-03-28 11:37:15 +0200

    tp reactjs

---
+++ screen-tps/fakedata.js
@@ -0,0 +1,80 @@
+export const fakeData =
+[{
+  "id": 1,
+  "img":"https://eastceylon.com/images/2021/07/15/cloth-1.png",
+  "name": "Black & white baby-cloth",
+  "price": 20,
+  "discount":2,
+  "discoutPrice":20-2/100*20,
+  "quantity":1,
+  "desc" :"Lorem ipusm quas a facere quibusdam porro qui consequuntur animi obcaecati repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. s lLorem ipsum dolor sit amet consectetur adipisicing elit. "
+}, {
+  "id": 2,
+  "img": "https://eastceylon.com/images/2021/07/15/cloth-2.png",
+  "name": "winnter comfortable baby-cloth",
+  "price": 40,
+  "discount":3,
+  "discoutPrice":40-3/100*40,
+  "quantity":1,
+  "desc" :"Lorem ipusm quas a facere quibusdam porro qui consequuntur animi obcaecati repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. s lLorem ipsum dolor sit amet consectetur adipisicing elit.  "
+}, {
+  "id": 3,
+   "img":"https://eastceylon.com/images/2021/07/15/cloth-3ec92b1b2b6634cc9.png",
+  "name": "cloth for 6 month baby",
+  "price": 30,
+  "discount":4,
+  "discoutPrice":30-4/100*30,
+  "quantity":1,
+  "desc" :"Lorem ipusm quas a facere quibusdam porro qui consequuntur animi obcaecati repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. s lLorem ipsum dolor sit amet consectetur adipisicing elit.  "
+
+}, {
+  "id": 4,
+  "img": "https://eastceylon.com/images/2021/07/15/cloth-4.png",
+  "name": "pants with smart-shirt",
+  "price": 80,
+  "discount":7,
+  "discoutPrice":80-8/100*80,
+  "quantity":1,
+  "desc" :"Lorem ipusm quas a facere quibusdam porro qui consequuntur animi obcaecati repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. s lLorem ipsum dolor sit amet consectetur adipisicing elit."
+},{
+    "id": 5,
+    "img": "https://eastceylon.com/images/2021/07/15/cloth-5.png",
+    "name": "summer-cloth for little baby",
+    "price": 35,
+    "discount":4,
+    "discoutPrice":35-4/100*35,
+    "quantity":1,
+    "desc" :"Lorem ipusm quas a facere quibusdam porro qui consequuntur animi obcaecati repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. s lLorem ipsum dolor sit amet consectetur adipisicing elit. "
+  
+  },  {
+    "id": 6,
+    "img": "https://eastceylon.com/images/2021/07/15/cloth-6.png",
+    "name": "green pants with cloth",
+    "price": 20,
+    "discount":2,
+    "discoutPrice":20-2/100*20,
+    "quantity":1,
+    "desc" :"Lorem ipusm quas a facere quibusdam porro qui consequuntur animi obcaecati repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. s lLorem ipsum dolor sit amet consectetur adipisicing elit. "
+  
+  }, 
+  {
+    "id": 7,
+    "img": "https://eastceylon.com/images/2021/07/15/cloth-7.png",
+    "name": "round-shirt for little girl",
+    "price": 50,
+    "discount":5,
+    "discoutPrice":50-5/100*50,
+    "quantity":1,
+    "desc" :"Lorem ipusm quas a facere quibusdam porro qui consequuntur animi obcaecati repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. s lLorem ipsum dolor sit amet consectetur adipisicing elit."
+    }, {
+    "id": 8,
+    "img": "https://eastceylon.com/images/2021/07/15/cloth-8.png",
+    "name": "summer white clothe with pants",
+    "price": 70,
+    "discount":6,
+    "discoutPrice":70-6/100*70,
+    "quantity":1,
+    "desc" :"Lorem ipusm quas a facere quibusdam porro qui consequuntur animi obcaecati repellat.Lorem ipsum dolor sit amet consectetur adipisicing elit. s lLorem ipsum dolor sit amet consectetur adipisicing elit. "
+  
+  }
+]
